616a969fbed856fb0323ce6e3fefbd896e5df3f4
wildansyahrul22 <wildansyahrulnurcholis@gmail.com>
2025-01-09 14:59:46 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    jsxInject: `import React from 'react'`, // jika menggunakan JSX secara eksplisit
-  },
   base: "/",
 });
